f7cf45ed525e23805cae75895eb1078a3df08328
= <=>
2019-05-13 11:47:25 +0800

    init project

---
+++ admin/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<style>
+html,
+body,
+ul,
+li,
+div,
+span,
+p,
+input,
+font {
+  padding: 0;
+  margin: 0;
+}
+</style>
